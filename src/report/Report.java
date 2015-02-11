aff2d5ade863d4004298b911a06e83d36b28d3eb
BREAK350 <break350@gmail.com>
2015-02-11 15:38:55 +0200

    зробив експорт в odt

--- src/report/Report.java
+++ src/report/Report.java
@@ -11,6 +11,7 @@ import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrintManager;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.oasis.JROdtExporter;
 import accounts.model.Account;
 
 public class Report {
@@ -55,6 +56,23 @@ public class Report {
 	}
 
 	public void exportToODT(ObservableList<Account> data, File file) {
-
+		String sourceFileName = "report.jasper";
+		String printFileName = null;
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		try {
+			JRDataSource dataSource = new AccountDataSource(data);
+			JROdtExporter docxExporter = new JROdtExporter();
+			printFileName = JasperFillManager.fillReportToFile(sourceFileName,
+					parameters, dataSource);
+			if (printFileName != null) {
+				docxExporter.setParameter(JRExporterParameter.INPUT_FILE_NAME,
+						printFileName);
+				docxExporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME,
+						file.getAbsolutePath());
+				docxExporter.exportReport();
+			}
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
 	}
 }
